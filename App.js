c9b37e58e8a8d2d8033dfef55ff5f2333ad63e88
LUIS HENRIQUE LEAL RIES <lhries@fpap166748.fspoa.br>
2019-09-28 16:57:48 -0300

    Listando as tarefas na tela inicial

---
+++ App.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import {StyleSheet, FlatList, Text, View} from 'react-native';
+
+const Item = ({title}) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemText}>{title}</Text>
+    </View>
+);
+
+export default class App extends Component {
+  tarefas = [
+    { id:1, tarefa:'Tarefa 1' },
+    { id:2, tarefa:'Tarefa 2' },
+    { id:3, tarefa:'Tarefa 3' },
+    { id:4, tarefa:'Tarefa 4' },
+    { id:5, tarefa:'Tarefa 5' },
+    { id:6, tarefa:'Tarefa 6' },
+  ];
+  
+
+  render() {
+    return (
+      <View style={styles.container}> 
+        <FlatList style={styles.list}
+          data = {this.tarefas}
+          keyExtractor = {item => item.id}
+          renderItem={
+            ({item}) => <Item title={item.tarefa} />
+          }
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     marginTop:30,
+     backgroundColor: '#FAFAFA',
+   },
+   list: {
+     padding: 20,
+   },
+   itemContainer:{
+    backgroundColor:'#FFF',
+    borderWidth: 1,
+    borderColor:'#DDD',
+    borderRadius: 5,
+    padding: 20
+   },
+   itemText:{
+     fontSize:20,
+     fontWeight:'bold',
+     color:'#333'
+   }
+});
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
